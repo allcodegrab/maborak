ea1650181b2d843866d7b4bb12330093a96ce9d4
maborak <maborak@65e6ca7b-ee30-0410-b29c-4faee72459fe>
2008-02-21 17:57:51 +0000

    Removed file/folder

--- trunk/maborak/samples/tutoriales/ajax/ejemplo1_server.php
+++
@@ -1,6 +0,0 @@
-<?php
-echo "variables  POST:\n";
-print_r($_POST);
-echo "variables  GET:\n";
-print_r($_GET);
-?>
--- trunk/maborak/samples/tutoriales/ajax/ejemplo3_server.php
+++
@@ -1,9 +0,0 @@
-Vestibulum semper. <b>Nullam non odio</b>. Aliquam quam. Vestibulum semper.
-Aliquam quam. Vestibulum semper. <u>nullam non odio. Aliquam quam</u>.
-<ol>
-	<li>Blabla</li>
-	<li>Blabla</li>
-	<li>Blabla</li>
-	<li>Blabla</li>
-</ol>
-Nullam non odio. Aliquam quam. Vestibulum semper. Nullam non odio. Aliquam quam.
